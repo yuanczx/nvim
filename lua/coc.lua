382ed2381faac98a0b25357e682c0d2e550f1509
yuanczx <yuanczx@foxmail.com>
2022-10-08 08:46:22 +0800

    Update coc.lua

--- lua/coc.lua
+++ lua/coc.lua
@@ -5,13 +5,15 @@ function _G.check_back_space()
     return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s')
 end
 
-keyset("n", "gd", "<Plug>(coc-definition)", {silent = true})
-keyset("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
-keyset("n", "gi", "<Plug>(coc-implementation)", {silent = true})
-keyset("n", "gr", "<Plug>(coc-references)", {silent = true})
+local opts = {silen = true}
 
-keyset("x", "<leader>f", "<Plug>(coc-format-selected)", {silent = true})
-keyset("n", "<leader>f", "<Plug>(coc-format-selected)", {silent = true})
+keyset("n", "gd", "<Plug>(coc-definition)", opts)
+keyset("n", "gy", "<Plug>(coc-type-definition)", opts)
+keyset("n", "gi", "<Plug>(coc-implementation)", opts)
+keyset("n", "gr", "<Plug>(coc-references)", opts)
+
+keyset("x", "<leader>f", "<Plug>(coc-format-selected)", opts)
+keyset("n", "<leader>f", "<Plug>(coc-format-selected)", opts)
 
 -- 使用Tab切换代码提示
 vim.cmd([[
